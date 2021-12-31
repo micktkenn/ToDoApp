3861e43e1abf169707d7a61964fe46cf78412ef8
Milkessa  K. Tesso <62355155+micktkenn@users.noreply.github.com>
2021-12-31 14:41:42 +0300

    Update Configs.java

--- src/sample/Database/Configs.java
+++ src/sample/Database/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "dichone7";
-    protected String dbName = "todo";
+    protected String dbPass = "";
+    protected String dbName = "javatodoapp";
 }
